99bf2b6f6369bfded911a5b4a3c2e38044f19660
John <kujoknight@gmail.com>
2020-10-30 23:07:00 -0400

    Created initial player and enemy scripts

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    #region Enemy Stats
+    public int maxHealth = 10;
+    public int currentHealth = 10;
+    #endregion
+
+    #region Enemy Sprites
+    private Image enemySpriteImage;
+    private Sprite currentSprite;
+    #endregion
+
+    private void Awake()
+    {
+        enemySpriteImage = GetComponent<Image>();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Player Stats
+    public int maxHealth = 100;
+    public int currentHealth = 100;
+    public int score = 0;
+    #endregion
+
+    #region Player Sprites
+    private Image playerSpriteImage;
+    private Sprite currentSprite;
+    #endregion
+
+    private void Awake()
+    {
+        playerSpriteImage = GetComponent<Image>();
+    }
+}
